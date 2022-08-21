fb0b11f6739c39373c06da1ebbe3a49e9852e092
Ajeet Singh Raina <ajeetraina@Docker-Ajeet-Singh-Rainas-MacBook-Pro.local>
2022-08-21 19:22:44 +0530

    Added

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "The DevOps Journey",
   tagline: "Concepts | Tools | Techniques",
-  url: "https://devops.collabnix.com",
+  url: "https://collabnixdevops.collabnix.com",
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
