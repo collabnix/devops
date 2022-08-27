fc2139e015816b89945dbc067fac7bd63679df0a
rohith-raju <rohithraju488@gmail.com>
2022-08-27 19:55:41 +0530

    fixed edit page links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,15 +36,13 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/collabnix/devops/tree/master",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/collabnix/devops/tree/master",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
