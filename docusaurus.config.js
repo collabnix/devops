7b72d63c11e73043c52803a3b428c73dc1320f45
rohith-raju <rohithraju488@gmail.com>
2022-08-30 10:15:55 +0530

    removed links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -69,7 +69,7 @@ const config = {
           },
           { to: "/blog", label: "Blog", position: "left" },
           {
-            href: "https://github.com/facebook/docusaurus",
+            href: "https://github.com/collabnix/devops",
             label: "GitHub",
             position: "right",
           },
